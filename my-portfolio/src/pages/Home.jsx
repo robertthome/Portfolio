0d7b6bdd4a4f2c6d1fe9ab85f8d0b70f720f4229
robertthome <remedy42290@gmail.com>
2021-11-10 16:14:51 -0800

    hero section. Almost mobile friendly

--- my-portfolio/src/pages/Home.jsx
+++ my-portfolio/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 import propic from '../images/propic.jpeg'
 
 import './Home.css'
@@ -9,8 +10,8 @@ const Home = () => {
     <div className='hero-section'>
       <img src={propic} alt="Our hero" className='hero-pic' />
       <div className='hero-description'>
-      <h2 className='hero-name'>Robert Thome</h2>
-      <p>I am a Software Engineer with a background in Culinary Arts. This last year has been challenging but I am also fortunate since I finally had a chance to pursue a dream of mine, web development. The skills I have learned from my career as a chef, such as attention to detail and the ability to think under pressure, has helped me in my pursuit of a new career in tech. Though I am familiar with building full stack applications, my passion is in front-end development. My goal is to create beautiful and responsive websites that are user friendly. A future goal of mine is to attend college to receive a bachelor's degree in Computer Science.</p>
+      <h1 className='hero-name'>Robert Thome</h1>
+      <p className='hero-text'>I am a Software Engineer with a background in Culinary Arts. This last year has been challenging but I am also fortunate since I finally had a chance to pursue a dream of mine, web development. The skills I have learned from my career as a chef, such as attention to detail and the ability to think under pressure, has helped me in my pursuit of a new career in tech. Though I am familiar with building full stack applications, my passion is in front-end development. My goal is to create beautiful and responsive websites that are user friendly. A future goal of mine is to attend college to receive a bachelor's degree in Computer Science.</p>
       </div>
     </div>
   )
